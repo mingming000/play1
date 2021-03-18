6a999e03903d728a7950ada17fad7a45118e8ae2
mingming000 <80876785+mingming000@users.noreply.github.com>
2021-03-18 15:34:39 +0800

    Add files via upload

---
+++ dddd.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+data={'Name':["John","Anna","Peter","Linda"],'Location':["New York","Paris","Berlin","London"],'Age':[24,13,53,33]}
+data_pandas=pd.DataFrame(data)
+display(data_pandas)
+
+
+# In[ ]:
+
+
+
+
